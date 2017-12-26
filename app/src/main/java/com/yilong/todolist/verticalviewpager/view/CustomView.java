6a63496e38bb833e59562897503540da59481b9c
chenlei <52197521aA>
2017-12-26 17:13:58 +0800

    自定义view

---
+++ app/src/main/java/com/yilong/todolist/verticalviewpager/view/CustomView.java
@@ -0,0 +1,24 @@
+package com.yilong.todolist.verticalviewpager.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by sun on 2017/12/26.
+ */
+
+public class CustomView extends ScrollView {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
